29a3fa0b56035e5e908c1783b100aea41552b9a0
gitArtman <html2015@126.com>
2017-04-21 18:42:20 +0800

    FGD
    DGDFAASF

---
+++ ts_type.ts
@@ -0,0 +1,13 @@
+// 布尔值
+
+// 最基本的数据类型就是简单的true/false值，在JavaScript和TypeScript里叫做boolean（其它语言中也一样）。
+
+var isDone: boolean = false;
+// 数字
+
+// 和JavaScript一样，TypeScript里的所有数字都是浮点数。 这些浮点数的类型是number。 除了支持十进制和十六进制字面量，Typescript还支持ECMAScript 2015中引入的二进制和八进制字面量。
+
+var decLiteral: number = 6;
+var hexLiteral: number = 0xf00d;
+let binaryLiteral: number = 0b1010;
+octalLiteral: number = 0o744;
